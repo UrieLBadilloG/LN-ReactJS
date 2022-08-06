bd4c6902ea0d819853507716b69ac65fb2b59825
UrieLBadilloG <ubadillog1600@alumno.ipn.mx>
2022-08-06 15:20:03 -0500

    Agregando error y loading

--- to-do/src/App/AppUI.js
+++ to-do/src/App/AppUI.js
@@ -8,6 +8,8 @@ import { CreateTodoButton } from "../CreateTodoButton";
 function AppUI(
     
 {       
+        loading, 
+        error, 
         totalTodos,
         completedTodos,
         searchValue,
@@ -33,6 +35,12 @@ function AppUI(
 
 
             {<TodoList>
+                {error && <p>Desesperate, hubo un error....</p>}
+
+                {loading && <p>Loading, no desesperes...</p>}
+
+                {(!loading && !serchedTodos.length) && <p>CREA TU PRIMER TO-DO</p>}
+
                 {serchedTodos.map(todo => (
                     <TodoItem
                         key={todo.text}
--- to-do/src/App/index.js
+++ to-do/src/App/index.js
@@ -9,33 +9,63 @@ import { AppUI } from './AppUI';
 // ];
 
 function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-  
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+  const[error, setError] = React.useState(false);
 
-  const [item, setItem] = React.useState(parsedItem);
+  const[loading, setLoading] = React.useState(true);
+
+
+  const [item, setItem] = React.useState(initialValue);
+
+
+  React.useEffect(()=>{
+    setTimeout(()=>{
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+  
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    }, 1000);
+  });
+
 
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
   };
 
-  return [
+  return {
     item,
     saveItem,
-  ];
+    loading, 
+    error,
+    
+  };
 }
 
 function App() {
   // Desestructuramos los datos que retornamos de nuestro custom hook, y le pasamos los argumentos que necesitamos (nombre y estado inicial)
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos, 
+    saveItem: saveTodos ,
+    loading,
+    error,
+  }= useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -67,18 +97,11 @@ function App() {
     saveTodos(newTodos);
   };
 
-  console.log('Render antes del useEffect');
-
-  React.useEffect(() => {
-    console.log('Use effect');
-  });
-
-  console.log('Render despues del useEffect');
-
-
   
   return (
     <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
